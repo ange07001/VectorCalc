99a0a8072a898ff40ac5c3deefc212098ae041c3
ange07001 <ange07001@utb.vaxjo.se>
2024-09-02 10:39:25 +0200

    Added some comments

--- VectorCalc.py
+++ VectorCalc.py
@@ -11,17 +11,20 @@ ComponentY = []
 
 NumberOfVectors = input("How many vectors do you want to calculate: ")
 
+#Appends input to lists, the pair of Vector and Angle of said Vector has the same index in the two different lists
 while RepsQuestions < int(NumberOfVectors):
     Vectors.append(input(f"Force of vector {RepsQuestions+1}: "))
     VectorsDeg.append(input(f"Angle of vector {RepsQuestions+1} in degrees: "))
     RepsQuestions = RepsQuestions + 1
     print("-"*25)
 
+#Calculates the components of all vectors
 for CurrentVector in Vectors:
     ComponentX.append(float(CurrentVector) * math.cos(math.radians(float(VectorsDeg[RepsComponents]))))
     ComponentY.append(float(CurrentVector) * math.sin(math.radians(float(VectorsDeg[RepsComponents]))))
     RepsComponents = RepsComponents + 1
 
+#Sums the components in both x and y directions
 for CurrentComponentX in ComponentX:
     TotComponentX = TotComponentX + CurrentComponentX
 
@@ -32,6 +35,7 @@ print(f"Total forces in X direction: {TotComponentX}")
 print(f"Total forces in Y direction: {TotComponentY}")
 print("-"*25)
 
+#Calculates the resultant and angle of the resultant
 Resultant = math.sqrt(TotComponentX ** 2 + TotComponentY ** 2)
 print(f"Resultant: {Resultant}")
 
